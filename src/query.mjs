c1a83bb63d8da73b568de2cb6f1dc79a8d2b91cb
Markus Felten <Markus.Felten@gmx.de>
2020-05-04 14:00:40 +0200

    fix: use recursive query to form cross-product

--- src/query.mjs
+++ src/query.mjs
@@ -10,40 +10,40 @@ export function DeclareVariable(name) {
 
 export function SymatemQueryMixin(base) {
   return class SymatemQueryMixin extends base {
-    *tripleQueries(tripleQueries = []) {
-      const types2Mask = {
-        "symbol:string:string": this.queryMasks.VMM,
-        "string:string:symbol": this.queryMasks.MMV,
-        "symbol:string:symbol": this.queryMasks.VMV,
-        "symbol:symbol:symbol": this.queryMasks.VVV
-      };
+    *tripleQueries(tripleQueries = [], initial = new Map()) {
+      if (tripleQueries.length === 0) {
+        yield initial;
+      } else {
+        const types2Mask = {
+          "symbol:string:string": this.queryMasks.VMM,
+          "string:string:symbol": this.queryMasks.MMV,
+          "symbol:string:symbol": this.queryMasks.VMV,
+          "symbol:symbol:symbol": this.queryMasks.VVV
+        };
 
-      const results = new Map();
+        const tripleQuery = tripleQueries.shift();
 
-      for (let tripleQuery of tripleQueries) {
         const mask = types2Mask[tripleQuery.map(s => typeof s).join(":")];
-        //console.log(tripleQuery);
 
         const query = tripleQuery.map(s => {
           if (typeof s === "symbol") {
-            const value = results.get(s);
+            const value = initial.get(s);
             return value === undefined ? this.symbolByName.Void : value;
           }
           return s;
         });
 
-        //console.log(mask, query);
-
         for (const r of this.queryTriples(mask, query)) {
+          const results = new Map(initial);
+
           tripleQuery.forEach((s, i) => {
             if (typeof s === "symbol" && query[i] === this.symbolByName.Void) {
               results.set(s, r[i]);
             }
           });
+          yield* this.tripleQueries(tripleQueries, results);
         }
       }
-
-      yield results;
     }
   };
 }
--- tests/query-test.mjs
+++ tests/query-test.mjs
@@ -4,28 +4,49 @@ import { prepareBackend } from "./helpers/util.mjs";
 import { DeclareVariable } from "SymatemQuery";
 
 test("tripleQueries", async t => {
-  const { backend, writer, a1, a2, a3, s1, s2, s3, s4 } = await prepareBackend({
+  const {
+    backend,
+    writer,
+    a1,
+    a2,
+    a3,
+    s1,
+    s2,
+    s3,
+    s4,
+    s5
+  } = await prepareBackend({
     a: 3,
-    s: 4
+    s: 5
   });
 
   writer.setTriple([s1, a1, s2], true);
   writer.setTriple([s2, a2, s3], true);
   writer.setTriple([s3, a3, s4], true);
+  writer.setTriple([s3, a3, s5], true);
 
-  for (const p of backend.tripleQueries([
+  const results = [];
+
+  for (const r of backend.tripleQueries([
     [s1, a1, DeclareVariable("A")],
     [DeclareVariable("A"), a2, DeclareVariable("B")],
     [DeclareVariable("B"), a3, DeclareVariable("C")]
   ])) {
-    console.log(p);
-    t.deepEqual(
-      p,
-      new Map([
-        [DeclareVariable("A"), s2],
-        [DeclareVariable("B"), s3],
-        [DeclareVariable("C"), s4]
-      ])
-    );
+    results.push(r);
   }
+
+  console.log(results);
+
+  t.deepEqual(results, [
+    new Map([
+      [DeclareVariable("A"), s2],
+      [DeclareVariable("B"), s3],
+      [DeclareVariable("C"), s4]
+    ]),
+    new Map([
+      [DeclareVariable("A"), s2],
+      [DeclareVariable("B"), s3],
+      [DeclareVariable("C"), s5]
+    ])
+  ]);
 });
